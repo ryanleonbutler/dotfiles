c58cafaa425cb11d97229920ffdf475e6c96839f
Ryan Butler <butryan@amazon.com>
2022-04-29 15:12:24 +0200

    updates

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,8 +2,26 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# function fish_greeting
+#     echo The time is (set_color yellow; date +%T; set_color normal) and this machine is called $hostname
+# end
+
 function fish_greeting
-    echo The time is (set_color yellow; date +%T; set_color normal) and this machine is called $hostname
+    echo '                 '(set_color F00)'___
+  ___======____='(set_color FF7F00)'-'(set_color FF0)'-'(set_color FF7F00)'-='(set_color F00)')
+/T            \_'(set_color FF0)'--='(set_color FF7F00)'=='(set_color F00)')    '(set_color red)(whoami)'@'(hostname)'
+[ \ '(set_color FF7F00)'('(set_color FF0)'0'(set_color FF7F00)')   '(set_color F00)'\~    \_'(set_color FF0)'-='(set_color FF7F00)'='(set_color F00)')'(set_color yellow)'    Uptime: '(set_color white)(uptime | sed 's/.*up \([^,]*\), .*/\1/')(set_color red)'
+ \      / )J'(set_color FF7F00)'~~    \\'(set_color FF0)'-='(set_color F00)')    IP Address: '(set_color white)(ipconfig getifaddr en0)(set_color red)'
+  \\\\___/  )JJ'(set_color FF7F00)'~'(set_color FF0)'~~   '(set_color F00)'\)     '(set_color yellow)'Version: '(set_color white)(echo $FISH_VERSION)(set_color red)'
+   \_____/JJJ'(set_color FF7F00)'~~'(set_color FF0)'~~    '(set_color F00)'\\
+   '(set_color FF7F00)'/ '(set_color FF0)'\  '(set_color FF0)', \\'(set_color F00)'J'(set_color FF7F00)'~~~'(set_color FF0)'~~     '(set_color FF7F00)'\\
+  (-'(set_color FF0)'\)'(set_color F00)'\='(set_color FF7F00)'|'(set_color FF0)'\\\\\\'(set_color FF7F00)'~~'(set_color FF0)'~~       '(set_color FF7F00)'L_'(set_color FF0)'_
+  '(set_color FF7F00)'('(set_color F00)'\\'(set_color FF7F00)'\\)  ('(set_color FF0)'\\'(set_color FF7F00)'\\\)'(set_color F00)'_           '(set_color FF0)'\=='(set_color FF7F00)'__
+   '(set_color F00)'\V    '(set_color FF7F00)'\\\\'(set_color F00)'\) =='(set_color FF7F00)'=_____   '(set_color FF0)'\\\\\\\\'(set_color FF7F00)'\\\\
+          '(set_color F00)'\V)     \_) '(set_color FF7F00)'\\\\'(set_color FF0)'\\\\JJ\\'(set_color FF7F00)'J\)
+                      '(set_color F00)'/'(set_color FF7F00)'J'(set_color FF0)'\\'(set_color FF7F00)'J'(set_color F00)'T\\'(set_color FF7F00)'JJJ'(set_color F00)'J)
+                      (J'(set_color FF7F00)'JJ'(set_color F00)'| \UUU)
+                       (UU)'(set_color normal)
 end
 
 ulimit -n 10240
@@ -34,9 +52,9 @@ alias starrc "vim $HOME/.local/share/chezmoi/dot_config/starship.toml"
 alias sshrc "vim ~/.ssh/config"
 alias vimrc "vim $HOME/.local/share/chezmoi/dot_config/lvim/config.lua"
 alias kittyrc "vim $HOME/.local/share/chezmoi/dot_config/kitty/kitty.conf"
-alias yabairc "vim $HOME/.local/share/chezmoi/dot_config/kitty/kitty.conf"
-alias skhdrc "vim $HOME/.local/share/chezmoi/dot_config/kitty/kitty.conf"
-alias spacebarrc "vim $HOME/.local/share/chezmoi/dot_config/kitty/kitty.conf"
+alias yabairc "vim $HOME/.local/share/chezmoi/executable_dot_yabairc"
+alias skhdrc "vim $HOME/.local/share/chezmoi/dot_skhdrc"
+alias spacebarrc "vim $HOME/.local/share/chezmoi/dot_config/spacebar/executable_spacebarrc"
 alias filesopen "sudo lsof -n | cut -f1 -d  | uniq -c | sort | tail"
 alias chrome "open -n -a /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --user-data-dir /tmp/chrome_dev_test --disable-web-security --no-sandbox && cp ~/Library/Application\ Support/Google/Chrome/NativeMessagingHosts/amazon_enterprise_access.json /tmp/chrome_dev_test/NativeMessagingHosts/"
 alias secretsrc "vim ~/.env"
