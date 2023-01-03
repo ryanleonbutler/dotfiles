540c3bf562b8ff60871ed281832f06b8c27d9e07
Ryan Butler <butryan@amazon.com>
2023-01-03 23:26:50 +0200

    updated neovim colorcolumn

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -8,6 +8,7 @@ function ColorMyPencils(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#555555" })
 end
 
 ColorMyPencils()
