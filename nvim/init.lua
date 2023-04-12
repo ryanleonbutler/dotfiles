a2676fdbc30b504e6aac76ffc5f9c97daef930ba
ryanleonbutler <ryanleonbutler@gmail.com>
2023-04-12 23:02:34 +0200

    updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,10 @@
--- ==============================================================================================================
+-- ==============================================================================================================ini
 -- [[ My Config ]]
 -- ==============================================================================================================
 
 -- ==============================================================================================================
 -- [[ globals ]]
+
 local set = vim.opt
 local g = vim.g
 local cmd = vim.cmd
@@ -89,6 +90,36 @@ require("lazy").setup({
         "folke/tokyonight.nvim",
         priority = 1000,
         config = function()
+            require("tokyonight").setup({
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+                light_style = "day", -- The theme is used when the background is set to light
+                transparent = true, -- Enable this to disable setting the background color
+                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+                styles = {
+                    -- Style to be applied to different syntax groups
+                    -- Value is any valid attr-list value for `:help nvim_set_hl`
+                    comments = { italic = true },
+                    keywords = { italic = true },
+                    functions = {},
+                    variables = {},
+                    -- Background styles. Can be "dark", "transparent" or "normal"
+                    sidebars = "transparent", -- style for sidebars, see below
+                    floats = "dark", -- style for floating windows
+                },
+                sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+                day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+                hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+                dim_inactive = false, -- dims inactive windows
+                lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+                --- You can override specific color groups to use other groups or a hex color
+                --- function will be called with a ColorScheme table
+                on_colors = function(colors) end,
+                --- You can override specific highlights to use other groups or a hex color
+                --- function will be called with a Highlights and ColorScheme table
+                on_highlights = function(highlights, colors) end,
+            })
             cmd.colorscheme("tokyonight-night")
         end,
     },
@@ -190,7 +221,6 @@ require("lazy").setup({
         event = "VeryLazy",
         dependencies = "nvim-treesitter/nvim-treesitter",
     },
-
     { "tpope/vim-surround" },
     { "tpope/vim-repeat" },
     {
@@ -211,6 +241,8 @@ require("lazy").setup({
             })
         end,
     },
+    { "ThePrimeagen/harpoon" },
+    { "ThePrimeagen/vim-be-good" },
 
     -- { import = 'custom.plugins' },
 }, {})
@@ -220,9 +252,7 @@ require("lazy").setup({
 
 -- colorcolumn
 set.colorcolumn = "88"
-
--- Set highlight on search
-set.hlsearch = true
+vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#555555" })
 
 -- Make line numbers default
 set.number = true
@@ -246,6 +276,8 @@ set.ignorecase = true
 set.smartcase = true
 set.incsearch = true
 set.smartindent = true
+-- Set highlight on search
+set.hlsearch = false
 
 -- Keep signcolumn on by default
 set.signcolumn = "yes"
@@ -754,5 +786,16 @@ null_ls.setup({
         }),
     },
 })
+
+-- ==============================================================================================================
+-- [[ harpoon setup ]]
+
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+map("n", "<leader>m", mark.toggle_file)
+map("n", "<leader>,", ui.toggle_quick_menu)
+
+-- ==============================================================================================================
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
