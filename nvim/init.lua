b0a92096654451e9b1f0cdeafbd8b38e05571d86
ryanleonbutler <ryanleonbutler@gmail.com>
2023-07-07 11:27:52 +0200

    updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -367,6 +367,26 @@ require("lazy").setup({
             })
         end,
     },
+    {
+        "akinsho/toggleterm.nvim",
+        lazy = false,
+        version = "*",
+        cmd = { "ToggleTerm", "TermExec" },
+        opts = {
+            size = 10,
+            on_create = function()
+                vim.opt.foldcolumn = "0"
+                vim.opt.signcolumn = "no"
+            end,
+            open_mapping = [[<F7>]],
+            shading_factor = 2,
+            direction = "float",
+            float_opts = {
+                border = "curved",
+                highlights = { border = "Normal", background = "Normal" },
+            },
+        },
+    },
     -- code suggestions
     -- {
     --     dir = "~/development/codewhisperer",
