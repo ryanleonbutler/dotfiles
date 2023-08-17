a22d7560e26f600313deac2b00cccd3d066d5ff4
ryanleonbutler <ryanleonbutler@gmail.com>
2023-08-17 22:23:44 +0200

    updates

--- fish/config.fish
+++ fish/config.fish
@@ -141,11 +141,17 @@ set FZF_DEFAULT_OPTS "--bind=shift-tab:down,tab:up"
 # 	--color=spinner:#f6c177,info:#9ccfd8,separator:#403d52 \
 # 	--color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
 
-# catppuccin
+# catppuccin-mocha
+# set -Ux FZF_DEFAULT_OPTS "\
+# --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+# --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+# --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+
+# catppuccin-frappe
 set -Ux FZF_DEFAULT_OPTS "\
---color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
---color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
---color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+--color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 \
+--color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
+--color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284"
 
 # Source secrets from .env
 set -gx SECRETS $HOME/.env
